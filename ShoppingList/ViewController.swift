9e7c898448ecaab09f72b2f63cc4d3258e88b4ab
Nikita <nvnagornyi@gmail.com>
2025-08-03 16:01:35 +0300

    Initial Commit

---
+++ ShoppingList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoppingList
+//
+//  Created by Никита Нагорный on 03.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
